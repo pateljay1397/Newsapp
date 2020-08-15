351d19e6ba86a97e48ff9ed3d9d7a41de3de1d0f
Jay Patel <pateljaypj1397@gmail.com>
2020-08-15 15:10:33 -0400

    State added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 import { getBitcoinArticles } from "./api";
 
-function App() {
-  getBitcoinArticles();
-  return (
-    <div>Write your code here for further implementaion</div>
-  );
+class App extends React.Component{
+state= {
+articles: [],
+apiError: "" 
+};
+async componentDidMount() {
+  try {
+    const response = await getBitcoinArticles();
+    this.setState({ articles: response.articles });
+  } catch (error) {
+    this.setState({ apiError: "Could not find any articles" });
+  }
+} 
+  render () {
+    return <div>Write your code here for further implementaion</div>;
+    }
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
 import { NEWS_API_KEY } from "./config";
 
 export const getBitcoinArticles = async () => {
-  try {
     const response = await fetch(
       `https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
     );
     const json = await response.json();
     console.log(json);
-  } catch (error) {
-    console.log(error);
-  }
-};
+}
+
+async componentDidMount() {
+    try {
+      const response = await getBitcoinArticles();
+      this.setState({ articles: response.articles });
+    } catch (error) {
+      this.setState({ apiError: "Could not find any articles" });
+    }
+  };
